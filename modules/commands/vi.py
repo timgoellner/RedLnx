bfd885f0243735e288ec1bebb1de035301826e4a
SirRoti <timgoellner6@gmail.com>
2025-02-28 22:05:15 +0100

    finished enter functionality for vi

--- modules/commands/vi.py
+++ modules/commands/vi.py
@@ -1,5 +1,8 @@
-import os, sys
+import os
 import modules.system as system, modules.data_handling as data_handling, modules.file_manager as file_manager, modules.perm_manager as perm_manager
+
+TERMINAL_LINES = os.get_terminal_size().lines
+TERMINAL_COLUMNS = os.get_terminal_size().columns
 
 
 def run(args: list[str], sudo: bool):
@@ -36,9 +39,8 @@ def run(args: list[str], sudo: bool):
   if not perm_manager.validate_directory_access(file_path) and not perm_manager.validate_session(): return
 
   data = file_manager.read_file(file_path, file_name)
-  terminal_lines = os.get_terminal_size().lines
 
-  unused_lines = terminal_lines - len(data) - 2
+  unused_lines = TERMINAL_LINES - len(data) - 2
   if unused_lines < 0:
     system.out(["vi: file is too long for console size"])
     return
@@ -49,10 +51,126 @@ def run(args: list[str], sudo: bool):
   system.out(['&1~\n&f' * unused_lines], colors=True, end_newline=False)
 
   characters = sum(len(line) for line in data)
-  system.out([f"\"{file_name}\" {len(data)} line, {characters} characters"])
+  system.out([f"\"{file_name}\" {len(data)} lines, {characters} characters"])
 
-  system.position(0, 0)
+  command_mode = True
+
+  position = [0, 0]
+  system.position(position)
+
 
   while True:
-    pass
+    key = system.get()
 
+    if not command_mode:
+      if len(key) == 1:
+        if position[0] >= (TERMINAL_COLUMNS - 1):
+          set_status('error: x boundary reached', position)
+          continue
+
+        system.out([key], end_newline=False, instant=True)
+        data[position[1]] += key
+        position[0] += 1
+
+
+      elif key == 'enter':
+        if unused_lines == 0:
+          set_status('error: y boundary reached', position)
+          continue
+
+        line_chars = data[position[1]][position[0]:]
+        data[position[1]] = data[position[1]][:position[0]]
+        system.out([' ' * len(line_chars)], instant=True)
+
+        position[0] = 0
+
+        data.insert(position[1] + 1, "")
+        for move_index in range(0, TERMINAL_LINES - unused_lines - 3 - position[1]):
+          line = TERMINAL_LINES - unused_lines - move_index - 2
+          system.position([position[0], line])
+          if len(data) > line + 1: system.out([data[line] + (' ' * (max(0, len(data[line + 1]) - len(data[line]))))], instant=True)
+          else: system.out([data[line]], instant=True)
+
+        unused_lines -= 1
+        position[1] += 1
+        system.position(position)
+
+        data[position[1]] = line_chars
+        if len(data) > position[1] + 1: system.out([line_chars + (' ' * max(0, len(data[position[1]+1]) - len(line_chars)))], instant=True)
+        elif len(line_chars) > 0: system.out([line_chars], instant=True)
+        else: system.out([' '], instant=True)
+
+        system.position(position)
+
+      elif key == 'backspace':
+        if position[0] == 0:
+          if position[1] == 0: continue
+
+          unused_lines += 1
+          system.out(['&1~\n&f'], colors=True, end_newline=False, instant=True)
+          data.pop()
+
+          position[1] -= 1
+          position[0] = len(data[position[1]])
+          system.position(position)
+          continue
+
+        position[0] -= 1
+        system.position(position)
+        system.out([' '], instant=True)
+        data[position[1]] = data[position[1]][:-1]
+        system.position(position)
+
+
+      elif key == 'up':
+        if position[1] == 0: continue
+
+        position[1] -= 1
+        position[0] = min(len(data[position[1]]), position[0])
+        system.position(position)
+
+      elif key == 'left':
+        if position[0] == 0:
+          if position[1] == 0: continue
+
+          position[1] -= 1
+          position[0] = len(data[position[1]])
+          system.position(position)
+          continue
+
+        position[0] -= 1
+        system.position(position)
+
+      elif key == 'down':
+        if position[1] == (TERMINAL_LINES - unused_lines - 3): continue
+
+        position[1] += 1
+        position[0] = min(len(data[position[1]]), position[0])
+        system.position(position)
+
+      elif key == 'right':
+        if position[0] == len(data[position[1]]):
+          if position[1] == (TERMINAL_LINES - unused_lines - 3): continue
+
+          position[1] += 1
+          position[0] = 0
+          system.position(position)
+          continue
+
+        if position[0] < len(data[position[1]]):
+          position[0] += 1
+          system.position(position)
+
+
+    elif key == ':':
+      pass
+    
+    elif key == 'a':
+      set_status(' -- insert -- ', position)
+      command_mode = False
+
+
+def set_status(status: str, position: list[int]):
+  system.position([0, TERMINAL_LINES - 2])
+  system.out([status + (' ' * (TERMINAL_COLUMNS - (len(status) + 1)))], instant=True)
+  system.position(position)   
--- modules/system.py
+++ modules/system.py
@@ -5,17 +5,39 @@ from colorama import Fore as color
 COLORCODES = {"&2": color.GREEN, "&f": color.WHITE, "&4": color.RED, "&e": color.YELLOW, "&1": color.BLUE, "&9": color.LIGHTBLUE_EX, "&a": color.LIGHTGREEN_EX}
 GHANDLE = ctypes.windll.kernel32.GetStdHandle(c_long(-11))
 
-def out(out_text: list[str], input_mode: bool = False, end_newline: bool = True, colors: bool = False) -> str:
+def out(out_text: list[str], input_mode: bool = False, end_newline: bool = True, colors: bool = False, instant: bool = False) -> str:
     for out_line_idx, out_line in enumerate(out_text):
         if colors:
             for text_color in COLORCODES.keys(): out_line = out_line.replace(text_color, COLORCODES.get(text_color))
-        for out_char in out_line:
-            msvcrt.putwch(out_char)
-            sleep(.001)
+        if instant:
+            for out_char in out_line:
+                msvcrt.putwch(out_char)
+        else:
+            for out_char in out_line:
+                msvcrt.putwch(out_char)
+                sleep(.001)
         if out_line_idx != len(out_text)-1: msvcrt.putwch('\n')
 
     if input_mode: return input()
     elif end_newline: msvcrt.putwch('\n')
+
+
+def get() -> str:
+    key = msvcrt.getch()
+
+    if key in [b'\x00', b'\xe0']:
+        key = msvcrt.getch()
+
+        if key == b'H': return 'up'
+        elif key == b'K': return 'left'
+        elif key == b'P': return 'down'
+        elif key == b'M': return 'right'
+
+        key = b''
+    elif key == b'\r': return 'enter'
+    elif key == b'\x08': return 'backspace'
+
+    return key.decode('utf-8', 'ignore')
 
 
 def sleep(duration: float):
@@ -24,6 +46,6 @@ def sleep(duration: float):
     while now < end: now = time.perf_counter()
 
 
-def position(x: int, y: int):
-    position = x + (y << 16)
+def position(position: list[int]):
+    position = position[0] + (position[1] << 16)
     ctypes.windll.kernel32.SetConsoleCursorPosition(GHANDLE, c_ulong(position))
